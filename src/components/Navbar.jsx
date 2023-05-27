5d37d508614ae94222b17b7652e41ab72fc5ab51
Ranjan-Bhat <109158234+Ranjan-Bhat@users.noreply.github.com>
2023-05-27 16:00:24 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
+    const [search, setSearch] = useState('');
   return (
     <nav>
         <Link to="/tv-shows/avengers">Avengers</Link>
+        <Link to="/tv-shows/pokemon">Pokemon</Link>
+        <Link to="/tv-shows/doraemon">Doraemon</Link>
+        <Link to={`/tv-shows/${search}`} >
+            <input type="text" placeholder='Search here...' value={search} onChange={e => setSearch(e.target.value)}/>
+        </Link>
     </nav>
   )
 }
